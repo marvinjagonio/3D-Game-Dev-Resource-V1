20814c3e37eed7a68a1da2143b77df83970459fd
marvin-git-repo <marvin-git-repo@gmail.com>
2024-11-29 09:25:19 +0800

    modified Player and added access to animator

--- Assets/Scripts/Basic3DAnimation/Player.cs
+++ Assets/Scripts/Basic3DAnimation/Player.cs
@@ -6,9 +6,23 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     Animator playerAnim;
+    Rigidbody playerRb;
+    [SerializeField] float playerMovementSpeed;
+
+    private Animator animator;
 
     private void Start()
     {
-        playerAnim = GetComponent<Animator>();
+        //playerAnim = GetComponent<Animator>();
+        playerRb = GetComponent<Rigidbody>();
+        animator = GetComponentInChildren<Animator>();
+        Debug.Log(animator);
     }
+
+    private void OnMove(InputValue inputvalue)
+    {
+        playerRb.velocity = inputvalue.Get<Vector3>() * playerMovementSpeed;
+    }
+
+
 }
