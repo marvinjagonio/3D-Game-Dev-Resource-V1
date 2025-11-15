e9f105dfbf8df84368f1e3bad9f0dbf1a8504b3a
marvin-git-repo <marvin-git-repo@gmail.com>
2025-11-15 12:10:53 +0800

    added third person controls

--- Assets/Melee Combat/Scripts/Player.cs
+++ Assets/Melee Combat/Scripts/Player.cs
@@ -1,16 +1,66 @@
+using Unity.Android.Gradle.Manifest;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public InputActionAsset InputActions;
+
+    private InputAction p_Move;
+
+
+    private CharacterController controller;
+    public Vector3 moveInput;
+    private Vector3 velocity;
+
+    [SerializeField] Transform cameraTransform;
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float gravity = -9.8f;
+    [SerializeField] private bool shouldFaceMoveDirection = false;
+
+    private void Awake()
     {
-        
+        p_Move = InputSystem.actions.FindAction("Move");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void OnEnable()
+    {
+        InputActions.FindActionMap("Player").Enable();
+    }
+
+    private void OnDisable()
+    {
+        InputActions.FindActionMap("Player").Disable();
+    }
+
+    private void Update()
+    {
+        moveInput = p_Move.ReadValue<Vector2>();
+
+        Vector3 forward = cameraTransform.forward;
+        Vector3 right = cameraTransform.right;
+
+        forward.y = 0;
+        right.y = 0;
+
+        forward.Normalize();
+        right.Normalize();
+
+        Vector3 moveDirection = forward * moveInput.y + right * moveInput.x;
+        controller.Move(moveDirection * speed * Time.deltaTime);
+
+        if (shouldFaceMoveDirection && moveDirection.sqrMagnitude > 0.001f)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(moveDirection, Vector3.up);
+            transform.rotation = Quaternion.Slerp(transform.rotation, toRotation, 10f * Time.deltaTime);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 }
